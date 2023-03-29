cd6503a4256c4c5108ee483f1a1e3eb0dfd35aff
william1234567889 <71872314+william1234567889@users.noreply.github.com>
2023-03-29 19:59:10 -0400

    backend can use ticketmaster API

---
+++ backend/collect_from_API.py
@@ -0,0 +1,15 @@
+import ticketpy
+
+tm_client = ticketpy.ApiClient('1m3jpoAZ65vufnoIEnQ47V5DjEoUGggG')
+
+pages = tm_client.events.find(
+    classification_name='Hip-Hop',
+    state_code='MA',
+    start_date_time='2023-05-19T00:00:00Z',
+    end_date_time='2023-05-21T20:00:00Z'
+)
+
+for page in pages:
+    for event in page:
+        print(event)
+        
